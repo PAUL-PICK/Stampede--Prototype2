a5597bcaa56fa963cbc47297b693d6c942170b55
paul pick <ppickone@outlook.com>
2023-03-04 23:42:49 +0000

    Projectile fixed
    made pizza shoot forward on space press

---
+++ Prototyp2/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MoveForward:MonoBehaviour {
+
+	public float speed = 10.0f;
+
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+		transform.Translate( Vector3.forward*speed*Time.deltaTime );
+		Debug.Log( "It fires" );
+
+	}
+}
---
+++ Prototyp2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController:MonoBehaviour {
+
+
+	[SerializeField] private float horizontallInput;
+	[SerializeField] private float speed = 30.0f;
+	[SerializeField] private GameObject projectilePrefab;
+	public float xBounds = 20.0f;
+
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+
+	// Update is called once per frame
+	void Update() {
+		// the if statments keep the player in bounds
+		if (transform.position.x<-xBounds) {
+			transform.position=new Vector3( -xBounds,transform.position.y,transform.position.z );
+		}
+		if (transform.position.x>xBounds) {
+			transform.position=new Vector3( xBounds,transform.position.y,transform.position.z );
+		}
+
+		horizontallInput=Input.GetAxis( "Horizontal" );
+		transform.Translate( Vector3.right*horizontallInput*speed*Time.deltaTime );
+
+		if (Input.GetKeyUp( KeyCode.Space )) {
+
+			Instantiate( projectilePrefab,transform.position,projectilePrefab.transform.rotation );
+		}
+
+	}
+
+}
